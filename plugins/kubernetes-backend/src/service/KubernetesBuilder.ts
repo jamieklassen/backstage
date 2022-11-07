c979b2ef8dfd6c3a9593489656dc462d5e04bba0
Ruben Vallejo <rvallejo@gmail.com>
2022-11-07 14:49:40 -0500

    Saved kubernetes builder with prettier formatting
    Signed-off-by: Ruben Vallejo <rvallejo@gmail.com>

--- plugins/kubernetes-backend/src/service/KubernetesBuilder.ts
+++ plugins/kubernetes-backend/src/service/KubernetesBuilder.ts
@@ -46,6 +46,7 @@ import {
 } from '@backstage/plugin-permission-common';
 import { NotAllowedError } from '@backstage/errors';
 import { kubernetesClusterReadPermission } from '@backstage/plugin-kubernetes-common';
+
 /**
  *
  * @alpha
